63bf238e1d9e929ad296fe5650f14367e1dc1d9f
Gabriel Leão <111463641+Gabriel-Leao51@users.noreply.github.com>
2024-09-25 23:17:57 -0300

    home finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './home/main/main.component';
+import { CursosGridComponent } from './home/cursos-grid/cursos-grid.component';
+import { SobreComponent } from './home/sobre/sobre.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MainComponent,
+    CursosGridComponent,
+    SobreComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/home/cursos-grid/cursos-grid.component.ts
@@ -0,0 +1,34 @@
+import { NgClass, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-grid',
+  standalone: true,
+  imports: [NgFor, NgClass],
+  templateUrl: './cursos-grid.component.html',
+  styleUrl: './cursos-grid.component.css',
+})
+export class CursosGridComponent {
+  cursos = [
+    {
+      titulo: 'Crianças',
+      imagem: 'assets/imagens/crianças.jpg',
+      descricao: 'Nunca é cedo demais para aprender!',
+    },
+    {
+      titulo: 'Adolescentes',
+      imagem: 'assets/imagens/adolescentes.jpg',
+      descricao: 'Desenvolvendo habilidades para o futuro!',
+    },
+    {
+      titulo: 'Adultos',
+      imagem: 'assets/imagens/adultos.jpg',
+      descricao: 'Expanda seus horizontes, domine o inglês!',
+    },
+    {
+      titulo: 'Professores',
+      imagem: 'assets/imagens/professores.jpg',
+      descricao: 'Aprimorando o ensino da língua inglesa.',
+    },
+  ];
+}
