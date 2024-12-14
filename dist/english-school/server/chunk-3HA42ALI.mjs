0bba226de6875e052ffb888c227ac7b1a94b87f2
Gabriel Leão <111463641+Gabriel-Leao51@users.noreply.github.com>
2024-12-13 23:34:10 -0300

    prerender removido

--- dist/english-school/server/chunk-3HA42ALI.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-OXCBM3KH.mjs";import"./chunk-Y2XTMLBF.mjs";import"./chunk-IMMMZWSA.mjs";import"./chunk-2Z63PRNR.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-PN6GMUDE.mjs";import"./chunk-QZK6HHLT.mjs";import"./chunk-OBIY6IM3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as CadastroComponent};
