252a788b752c0bf826a7e64a17a7ef70ed0617c4
Gabriel Leão <111463641+Gabriel-Leao51@users.noreply.github.com>
2024-12-16 21:14:14 -0300

    correção de imports

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 interface LoginResponse {
   user: any;
--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { Curso } from '../models/curso.model';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
